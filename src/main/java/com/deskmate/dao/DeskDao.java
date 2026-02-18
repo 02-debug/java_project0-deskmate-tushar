b6bbafa2148cadb344ef53d58ed55b72ff218d1d
02-debug <bhuretushar7@gmail.com>
2026-02-18 16:05:51 +0530

    DeskDao

--- src/main/java/com/deskmate/dao/DeskDao.java
+++ src/main/java/com/deskmate/dao/DeskDao.java
@@ -3,6 +3,8 @@ package com.deskmate.dao;
 import java.util.List;
 import java.util.Optional;
 
+import com.deskmate.model.Desk;
+
 public interface DeskDao {
     long insertDesk(String code, String name);
     void deactivateDesk(long deskId);

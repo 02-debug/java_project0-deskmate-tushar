febabed549c49016785d4e82061e34212c866c72
02-debug <bhuretushar7@gmail.com>
2026-02-18 15:56:12 +0530

    Add Services

---
+++ src/main/java/com/deskmates/services/DeskService.java
@@ -0,0 +1,42 @@
+package com.deskmates.services;
+
+import com.deskmate.dbps.dao.DeskDao;
+import com.deskmate.dbps.exception.EntityNotFoundException;
+import com.deskmate.dbps.exception.ValidationException;
+import com.deskmate.dbps.model.Desk;
+import com.deskmate.dbps.util.ValidationUtil;
+
+import java.util.List;
+
+public class DeskService {
+    private final DeskDao deskDao;
+
+    public DeskService(DeskDao deskDao) {
+        this.deskDao = deskDao;
+    }
+
+    public long addDesk(String code, String name) {
+        ValidationUtil.requireNonBlank(code, "deskCode");
+        ValidationUtil.requireNonBlank(name, "name");
+
+        if (deskDao.findByCode(code).isPresent()) {
+            throw new ValidationException("Desk code already exists: " + code);
+        }
+        return deskDao.insertDesk(code, name);
+    }
+
+    public void deactivateDesk(String code) {
+        Desk d = deskDao.findByCode(code)
+                .orElseThrow(() -> new EntityNotFoundException("Desk not found: " + code));
+        deskDao.deactivateDesk(d.getDeskId());
+    }
+
+    public List<Desk> listActive() {
+        return deskDao.listActive();
+    }
+
+    public Desk getByCodeOrThrow(String code) {
+        return deskDao.findByCode(code)
+                .orElseThrow(() -> new EntityNotFoundException("Desk not found: " + code));
+    }
+}
